7500e526e145372094c5ab4da409dd6dedf574a8
Albert Alises <albert.alises@gmail.com>
2018-10-05 16:24:37 +0200

    Added link to repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ interface footerProps {
 export default (props: footerProps) => {
   return (
     <footer className="footer fl ma3 f5 ttu tracked fw4 white-90 bg-black-50">
-      Made with {props.emoji} by <a target="_blank" className="dim white grow" href="something">{props.author}</a>
+      Made with {props.emoji} by <a target="_blank" className="dim white grow" href="https://github.com/aalises/random-kexp">{props.author}</a>
     </footer>
   )
 }
